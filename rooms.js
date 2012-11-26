ff892dc996f7414873160c9dc17f20d0cfbbca1c
Kook Mul <good@diginori.com>
2012-11-26 09:47:00 +0900

    첫 소스 커밋
    outsider 님 펴낸 node.js 프로그래밍 8장 Simple Chat 예제를 기반으로 room.js 에서 채팅 메시지에
    따라 북(b1.aif),장구(jgS3_80.aif),징(j7.aif),괭과리(kkM1.aif),추임세-잘한다(chu04.aif)
    를 iOS 의 콘설명령어인 afplay 를 이용하여 플레이 하게 만듬. 채팅창에서 각각 b,g,j,k,c 를 입력하면 소리가
    나는 구조임

---
+++ rooms.js
@@ -0,0 +1,87 @@
+var Chat = require('./chat');
+var PlayCount = 1;
+
+module.exports = function(app) {
+  var io = require('socket.io').listen(app);
+  
+  io.configure(function(){
+    io.set('log level', 3);
+    io.set('transports', [
+        'websocket'
+      , 'flashsocket'
+      , 'htmlfile'
+      , 'xhr-polling'
+      , 'jsonp-polling'
+    ]);
+  });
+
+  var Room = io
+    .of('/room')
+    .on('connection', function(socket) {
+      var joinedRoom = null;
+      socket.on('join', function(data) {
+        if (Chat.hasRoom(data.roomName)) {
+          joinedRoom = data.roomName;
+          socket.join(joinedRoom);
+          socket.emit('joined', {
+            isSuccess:true, nickName:data.nickName
+          });
+          socket.broadcast.to(joinedRoom).emit('joined', {
+            isSuccess:true, nickName:data.nickName
+          });
+          Chat.joinRoom(joinedRoom, data.nickName);
+        } else {
+          socket.emit('joined', {isSuccess:false});
+        }
+      });
+
+      socket.on('message', function(data) {
+        
+        console.log("data.msg:"+data.msg);
+
+        //digiNORI Start
+        var sys = require('sys')
+        var exec = require('child_process').exec;
+        function puts(error, stdout, stderr) { sys.puts(stdout) }
+        
+        if(data.msg == "c"){
+          exec("afplay ./sound/chu04.aif &", puts);
+        }
+
+        if(data.msg == "j"){
+          exec("afplay ./sound/j7.aif &", puts);
+        }
+
+        if(data.msg == "b"){
+          exec("afplay ./sound/b1.aif &", puts);
+        }
+
+        if(data.msg == "k"){
+          exec("afplay ./sound/kkM1.aif &", puts);
+        }
+
+        if(data.msg == "g"){
+          exec("afplay ./sound/jgS3_80.aif &", puts);
+        }
+        
+        PlayCount = PlayCount + 1;
+
+        console.log("PlayCount:"+PlayCount);
+        //digiNORI End
+
+        if (joinedRoom) {
+          socket.broadcast.to(joinedRoom).json.send(data);
+        } 
+      });
+
+      socket.on('leave', function(data) {
+        if (joinedRoom) {
+          Chat.leaveRoom(joinedRoom, data.nickName);
+          socket.broadcast.to(joinedRoom).emit('leaved', {
+            nickName:data.nickName
+          });
+          socket.leave(joinedRoom);
+        }
+      });
+    });
+}
